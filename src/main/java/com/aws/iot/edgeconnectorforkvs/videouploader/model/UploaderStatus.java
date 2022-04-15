17c4fde499f6136337bdf4486f06bdde79d46471
weichihl <63628733+weichihl@users.noreply.github.com>
2022-04-16 00:56:02 +0800

    Support getStatus in video uploader (#19)

---
+++ src/main/java/com/aws/iot/edgeconnectorforkvs/videouploader/model/UploaderStatus.java
@@ -0,0 +1,16 @@
+package com.aws.iot.edgeconnectorforkvs.videouploader.model;
+
+public enum UploaderStatus {
+    /**
+     * Uploader is stopped.
+     */
+    STOPPED,
+    /**
+     * Uploader is started.
+     */
+    STARTED,
+    /**
+     * Uploader is terminating.
+     */
+    TERMINATING
+}
